dc51ed2d3ddf60a5f595da92f7bb0555d4405af9
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-10-01 10:19:00 -0300

    create test to compare json paths

--- src/test/java/com/tidz/test_demo/todo/TodoJsonTest.java
+++ src/test/java/com/tidz/test_demo/todo/TodoJsonTest.java
@@ -1,8 +1,12 @@
 package com.tidz.test_demo.todo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+
+import com.jayway.jsonpath.JsonPath;
 
 public class TodoJsonTest {
 
@@ -26,6 +30,30 @@ public class TodoJsonTest {
 
 		JSONAssert.assertEquals(expected, data, false);
 
+	}
+
+	@Test
+	void shouldCompareJsonPath() {
+		var json = """
+					{
+					"todos": [
+						{
+							"name": "TEST 1",
+							"completed": true
+						},
+						{
+							"name": "TEST 2",
+							"completed": true
+						}
+					]
+				}
+				""";
+
+		Integer length = JsonPath.read(json, "$.todos.length()");
+		String name = JsonPath.read(json, "$.todos[1].name");
+
+		assertEquals(2, length);
+		assertEquals("TEST 2", name);
 	}
 
 	private String getRestData() {
